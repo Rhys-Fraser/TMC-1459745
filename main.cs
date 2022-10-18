ecbbc8b5c8541590869f1ae444782cafd5a91cc1
RhysFraser <116083655+Rhys-Fraser@users.noreply.github.com>
2022-10-18 13:46:18 +0000

    Added RangeTest

--- main.cs
+++ main.cs
@@ -1,15 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program {
-  public static void Main (string[] args) {
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] months = new string[12]
+        {"jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
 
-    string[] months = new string[12]
-    {"jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+        Random rnd = new Random();
 
-    Random rnd = new Random();
+        int month = rnd.Next(1, 13);
 
-    int month = rnd.Next(1, 13);
+        Console.WriteLine("Month is {0}", months[month - 1]);
+        RangeTest();
 
-    Console.WriteLine("Month is {0}", months[month-1]);
-  }
+    }
+
+    public static void RangeTest()
+    {
+        IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
+
+        foreach (int num in squares)
+        {
+            Console.WriteLine(num);
+        }
+    }
 }
