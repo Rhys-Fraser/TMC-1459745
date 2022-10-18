2f55d4a9aeb2be259781fd6470261925f77455d0
RhysFraser <116083655+Rhys-Fraser@users.noreply.github.com>
2022-10-18 13:03:55 +0000

    Created Random Month Generator

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    string[] months = new string[12]
+    {"jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+
+    Random rnd = new Random();
+
+    int month = rnd.Next(1, 13);
+
+    Console.WriteLine("Month is {0}", months[month-1]);
+  }
+}
